92f3e38f898bb893716d948af7a69a0e1114b0b5
jakbonful <johnalexbonful@gmail.com>
2026-03-26 14:06:47 +0000

    conditional encoder

--- COMBINATIONAL_CIRCUIT/8to3_encoder.v
+++ COMBINATIONAL_CIRCUIT/8to3_encoder.v
@@ -5,24 +5,18 @@ module encoder_8to3 (
     output reg [2:0] y
 );
 
+    integer  i;
+
     always @(*) begin
         if (enable == 0) begin
             y = 8'b0;
         end else begin
-            //case statement.check all the 8 valid combinations of d
-            case (d) 
-                8'b00000001 : y = 3'd0; 
-                8'b00000010 : y = 3'd1; 
-                8'b00000100 : y = 3'd2; 
-                8'b00001000 : y = 3'd3; 
-                8'b00010000 : y = 3'd4; 
-                8'b00100000 : y = 3'd5; 
-                8'b01000000 : y = 3'd6; 
-                8'b10000000 : y = 3'd7; 
-                // output has default value to prevent latch creation
-
-                default: y = 3'd0;
-            endcase
+            y = 3'b000; //default
+            for (i = 0; i < 8;i = i +1) begin
+                if (d[i]) begin
+                    y  = i[2:0];
+                end
+            end
         end
     end
     
