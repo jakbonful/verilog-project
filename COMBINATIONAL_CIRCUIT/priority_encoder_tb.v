c9b7feb817730306fb7083eb32349a1ae2e8890d
jakbonful <johnalexbonful@gmail.com>
2026-03-28 13:14:00 +0000

    Project files updated (ADDERS + COMBINATIONAL_CIRCUIT)

---
+++ COMBINATIONAL_CIRCUIT/priority_encoder_tb.v
@@ -0,0 +1,31 @@
+module  pri_enc_4to2_tb ();
+    //declare variables
+    reg [3:0] x;
+    wire [1:0] y;
+    wire v;
+
+
+    //instantiate the DUT
+    pri_enc_4to2 pri_ENC (
+        .a(x),
+        .q(y),
+        .x(v)
+    );
+
+
+    //monitor values
+    initial begin
+        $display("4 to 2 Priority Encoder");
+        $monitor("x = %b y = %b v = %b", x, y, v);
+    end
+
+    //generate random test
+    initial begin
+        repeat (10) begin
+            //generate an unsigned 32bit no. but bask to last 4 bits
+            x = $random & (4'b1111);
+            #10;
+        end
+    end
+    
+endmodule
