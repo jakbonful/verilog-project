8382ba4ada197874947d6c4962afde3dcc3b1e6b
jakbonful <johnalexbonful@gmail.com>
2026-03-31 19:57:35 +0000

    reset added

--- sequential_designs/d_latch.v
+++ sequential_designs/d_latch.v
@@ -1,6 +1,9 @@
+`timescale 1us/1ns
+
 module d_latch (
     input d,
     input enable,
+    input reset,   // active HIGH reset
     output q,
     output q_not
 );
@@ -8,12 +11,15 @@ module d_latch (
     reg dlatch;
 
     always @(*) begin
-        if (enable) begin
-            dlatch <= d;
-        end
+        dlatch = dlatch;        // default: hold value
+
+        if (reset)
+            dlatch = 1'b0;      // reset dominates
+        else if (enable)
+            dlatch = d;         // transparent when enabled
     end
 
     assign q = dlatch;
-    assign q_not = ~q;
-    
+    assign q_not = ~dlatch;
+
 endmodule
