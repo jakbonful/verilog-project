5af1eb2973f68a1cca39bfd32453aa8e435a42bd
jakbonful <johnalexbonful@gmail.com>
2026-04-12 10:59:46 +0000

    fibonacci

---
+++ FUNCTIONS_AND_TASKS/fibonacci.v
@@ -0,0 +1,27 @@
+`timescale 1us/1ns
+
+module fibonacci ();
+
+    //fibonacci sequence
+    function automatic integer fibonacci (input integer N);
+        begin
+            if (N == 0) begin
+                fibonacci = 0;
+            end 
+            else if (N == 1) begin
+                fibonacci = 1;
+            end 
+            else
+                fibonacci = fibonacci (N-1) + fibonacci(N-2);
+        end        
+    endfunction
+
+    initial begin
+        #1; $display($time, " fibonacci(5) = %d", fibonacci(5));
+        #1; $display($time, " fibonacci(9) = %d", fibonacci(9));
+        #1; $display($time, " fibonacci(80) = %d", fibonacci(7));
+        #1; $display($time, " fibonacci(6) = %d", fibonacci(6));
+        #1; $display($time, " fibonacci(10) = %d", fibonacci(10));
+    end
+
+endmodule
